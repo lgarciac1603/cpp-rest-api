aa8e2933f50473d99f08892063d132ff4b00b7aa
Fernando García <lfgc851@gmail.com>
2026-04-20 06:24:53 -0600

    example local file added

--- src/config/config.example.h
+++ src/config/config.example.h
@@ -1,10 +1,10 @@
-#pragma once
+﻿#pragma once
 
 #define DB_HOST "localhost"
 #define DB_PORT "5432"
-#define DB_NAME "apidb"
+#define DB_NAME ""
 #define DB_USER ""
 #define DB_PASS ""
 #define APP_PORT "8080"
-#define JWT_SECRET "dev-secret-key"
+#define JWT_SECRET ""
 #define CORS_ALLOW_ORIGIN "http://localhost:4200"
